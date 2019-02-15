286a0fa7cb26c12d85049c15edb8f94708a58d4e
ropenfold <stuartroper@gmail.com>
2019-02-15 13:52:48 +0000

    update js further

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var secondHand = document.getElementById("hour-hand");
-var minuteHand = document.getElementById("minute-hand");
-var secondHand = document.getElementById("second-hand");
-
-
 function setTime() {
   var time = new Date();
   var seconds = time.getSeconds();
@@ -10,25 +5,31 @@ function setTime() {
   var hours = time.getHours() % 12;
 
   setSeconds(seconds);
-  console.log(seconds);
-  setMinutes(minutes);
+  setMinutes(minutes, seconds);
   console.log(minutes);
-  setHours(hours);
+  setHours(hours, minutes);
   console.log(hours);
 }
 
 function setSeconds(seconds) {
   var secondDregrees = seconds * 6;
-  secondHand.style.transform = 'rotate (' + secondDregrees + ' )';
+  document
+    .getElementById("secondHand")
+    .setAttribute("style", `transform: rotate(${secondDregrees}deg)`);
 }
 
-function setMinutes(minutes) {
-
+function setMinutes(minutes, seconds) {
+  var minuteDegrees = minutes * 6 + 0.1 * seconds;
+  document
+    .getElementById("minuteHand")
+    .setAttribute("style", `transform: rotate(${minuteDegrees}deg)`);
 }
 
-function setHours(hours) {
-
-
+function setHours(hours, minutes) {
+  var hourDegrees = hours * 30 + 0.5 * minutes;
+  document
+    .getElementById("hourHand")
+    .setAttribute("style", `transform: rotate(${hourDegrees}deg)`);
 }
 
 setInterval(setTime, 1000);
